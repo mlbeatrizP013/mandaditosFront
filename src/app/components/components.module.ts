5b558593dc72ec3d305ab3407c0af69934336fc6
[luis ngel] <olaquehace568@gmail.com>
2025-07-29 15:22:31 -0600

    union de login de la app a la rama main

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { IonicModule } from '@ionic/angular';
 import { MainUserComponent } from './main-user/main-user.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
 MainUserComponent,
+LoginComponent
   ],
   imports: [
     CommonModule,
     IonicModule,
+     FormsModule
   ],
   exports: [
-
-MainUserComponent,
+LoginComponent,
+MainUserComponent
   ]
 })
 export class ComponentsModule { }
